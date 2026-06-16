869d6c4d785f5a41b4a622549947fbb35b0a7e5f
thiagomacena110 <thiago.macena.110@ufrn.edu.br>
2026-06-15 23:32:46 -0300

    Add files via upload

--- barbearia2.0.py
+++ barbearia2.0.py
@@ -1,33 +1,53 @@
-usuarios = []
-cortes = []
-horarios = []
+import pickle
+
+
+try:
+    arq = open("barbearia.dat", "rb")
+    dados = pickle.load(arq)
+
+    usuarios = dados["usuarios"]
+    cortes = dados["cortes"]
+    horarios = dados["horarios"]
+    agendamentos = dados["agendamentos"]
+
+    arq.close()
+
+except:
+    usuarios = {}
+    cortes = {}
+    horarios = {}
+    agendamentos = {}
 
 m = ""
-print("""Seja bem-vindo a Barbearia!
-    Aqui temos: 
-    Cadastro de usuário
-    Gerencia de cortes 
-    Agenda de cortes 
-    Relatórios
-    
-    """)
+
+print("""
+Seja bem-vindo à Barbearia!
+
+Aqui temos:
+- Cadastro de usuários
+- Gerência de cortes
+- Agenda de horários
+- Relatórios
+""")
 
 while m != "0":
-    
+
     print("""
-    ----- Sistema de Gestão para Barbearia -----
-    ----- 1 - Usuário --------------------------
-    ----- 2 - Estilos de cortes ----------------
-    ----- 3 - Agenda de Horários ---------------
-    ----- 4 - Relatório ------------------------
-    ----- 0 - Sair -----------------------------
-    """)
+----- Sistema de Gestão para Barbearia -----
+
+1 - Usuários
+2 - Cortes
+3 - Agenda de Horários
+4 - Relatório
+0 - Sair
+""")
 
     m = input("Escolha uma opção: ")
 
-    
-    # 1- Usuário
-    
+    # ==========================
+    # USUÁRIOS
+    # ==========================
+
     if m == "1":
 
         n = ""
@@ -35,66 +55,97 @@ while m != "0":
         while n != "0":
 
             print("""
-            ----- Módulo de Usuário -----
-            ----- 1 - Cadastrar usuário -
-            ----- 2 - Alterar usuário ---
-            ----- 3 - Listar usuários ---
-            ----- 4 - Remover usuário ---
-            ----- 0 - Voltar ------------
-            """)
+----- Módulo Usuários -----
+
+1 - Cadastrar usuário
+2 - Alterar usuário
+3 - Listar usuários
+4 - Remover usuário
+0 - Voltar
+""")
 
             n = input("Escolha uma opção: ")
 
-            # Para cadastrar. usando o append para adiocinar na lista 
+            # CADASTRAR
             if n == "1":
-                nome = input("Digite o nome do usuário: ")
-                usuarios.append(nome)
-                print("Usuário cadastrado com sucesso.")
 
-            # Alterar o usuário. o for é usando para olhar a lista e o len para contar quanto item na lista. o i para saber a posição. 
+                cpf = input("Digite o CPF: ")
+                nome = input("Digite o nome: ")
+                telefone = input("Digite o telefone: ")
+
+                usuarios[cpf] = [nome, telefone]
+
+                print("Usuário cadastrado com sucesso!")
+
+            # ALTERAR
             elif n == "2":
-                print("\nLista de usuários:")
-                
-                for i in range(len(usuarios)):
-                    print(i, "-", usuarios[i])
 
-                pos = int(input("Digite o número do usuário: "))
-                novo_nome = input("Digite o novo nome: ")
-            #pos é para mudar o nome na lista
-                usuarios[pos] = novo_nome
-
-                print("Usuário alterado com sucesso.")
-
-            # Lista de Usuário 
-            elif n == "3":
-                print("""
-                ----------------------------              
-                --- Usuários cadastrados ---
-                ----------------------------
-                """)
                 if len(usuarios) == 0:
-                    print("Nenhum usuário encontrado.")
+                    print("Nenhum usuário cadastrado.")
+
                 else:
-                    for usuario in usuarios:
-                        print(usuario)
 
-            # Remover usuário 
+                    for cpf in usuarios:
+                        print(
+                            "CPF:", cpf,
+                            "| Nome:", usuarios[cpf][0],
+                            "| Telefone:", usuarios[cpf][1]
+                        )
+
+                    cpf = input("Digite o CPF do usuário: ")
+
+                    if cpf in usuarios:
+
+                        nome = input("Digite o novo nome: ")
+                        telefone = input("Digite o novo telefone: ")
+
+                        usuarios[cpf] = [nome, telefone]
+
+                        print("Usuário alterado com sucesso!")
+
+                    else:
+                        print("CPF não encontrado.")
+
+            # LISTAR
+            elif n == "3":
+
+                if len(usuarios) == 0:
+                    print("Nenhum usuário cadastrado.")
+
+                else:
+
+                    print("\nUsuários cadastrados:")
+
+                    for cpf in usuarios:
+                        print(
+                            "CPF:", cpf,
+                            "| Nome:", usuarios[cpf][0],
+                            "| Telefone:", usuarios[cpf][1]
+                        )
+
+            # REMOVER
             elif n == "4":
-                print("\nLista de usuários:")
 
-                for i in range(len(usuarios)):
-                    print(i, "-", usuarios[i])
+                if len(usuarios) == 0:
+                    print("Nenhum usuário cadastrado.")
 
-                pos = int(input("Digite o número do usuário para remover: "))
+                else:
 
-                usuarios.pop(pos)
-                #usado o pop para remover (pela a posição)
+                    cpf = input("Digite o CPF do usuário: ")
 
-                print("Usuário removido com sucesso.")
+                    if cpf in usuarios:
 
-    
-    # 2- Estilos Cortes 
-  
+                        usuarios.pop(cpf)
+
+                        print("Usuário removido com sucesso!")
+
+                    else:
+                        print("CPF não encontrado.")
+
+    # ==========================
+    # CORTES
+    # ==========================
+
     elif m == "2":
 
         n = ""
@@ -102,49 +153,58 @@ while m != "0":
         while n != "0":
 
             print("""
-            ----- Estilos de Cortes -----
-            ----- 1 - Cadastrar corte ---
-            ----- 2 - Listar cortes -----
-            ----- 3 - Remover corte -----
-            ----- 0 - Voltar ------------
-            """)
+----- Módulo Cortes -----
+
+1 - Cadastrar corte
+2 - Listar cortes
+3 - Remover corte
+0 - Voltar
+""")
 
             n = input("Escolha uma opção: ")
 
-            # Cadastrar Corte
             if n == "1":
+
+                codigo = len(cortes) + 1
+
                 corte = input("Digite o nome do corte: ")
-                cortes.append(corte)
 
-                print("Corte cadastrado com sucesso.")
+                cortes[codigo] = corte
 
-            # Lista Cortes
+                print("Corte cadastrado com sucesso!")
+
             elif n == "2":
 
-                print("""
-                ----------------------------              
-                ---- Cortes cadastrados ----
-                ----------------------------
-                """)
+                if len(cortes) == 0:
+                    print("Nenhum corte cadastrado.")
 
-                for corte in cortes:
-                    print(corte)
+                else:
 
-            # Remover Corte
+                    print("\nCortes cadastrados:")
+
+                    for codigo in cortes:
+                        print(codigo, "-", cortes[codigo])
+
             elif n == "3":
 
-                for i in range(len(cortes)):
-                    print(i, "-", cortes[i])
+                if len(cortes) == 0:
+                    print("Nenhum corte cadastrado.")
 
-                pos = int(input("Digite o número do corte: "))
+                else:
 
-                cortes.pop(pos)
+                    for codigo in cortes:
+                        print(codigo, "-", cortes[codigo])
 
-                print("Corte removido.")
+                    codigo = int(input("Digite o código do corte: "))
 
-    
-    # 3- Agenda de Horários
-    
+                    cortes.pop(codigo)
+
+                    print("Corte removido com sucesso!")
+
+    # ==========================
+    # Agenda de Horários 
+    # ==========================
+
     elif m == "3":
 
         n = ""
@@ -152,78 +212,181 @@ while m != "0":
         while n != "0":
 
             print("""
-            ----- Agenda de Horários -----
-            ----- 1 - Cadastrar horário --
-            ----- 2 - Listar horários ----
-            ----- 3 - Remover horário ----
-            ----- 0 - Voltar -------------
-            """)
+----- Agenda de Horários -----
+
+1 - Cadastrar horário
+2 - Listar horários
+3 - Remover horário
+4 - Agendar corte
+0 - Voltar
+""")
 
             n = input("Escolha uma opção: ")
 
-            # Cadastrar Horário
             if n == "1":
+
+                codigo = len(horarios) + 1
 
                 horario = input("Digite o horário: ")
 
-                horarios.append(horario)
+                horarios[codigo] = horario
 
-                print("Horário cadastrado.")
+                print("Horário cadastrado com sucesso!")
 
-            # Lista de Horários
             elif n == "2":
 
-                print("""
-                ----------------------------              
-                --- Horários cadastrados ---
-                ----------------------------
-                """)
-       
-
                 if len(horarios) == 0:
-                    print("Nenhum horário encontrado.")
-                else:
-                    for horario in horarios:
-                        print(horario)
+                    print("Nenhum horário cadastrado.")
 
-            # Remover Horários 
+                else:
+
+                    print("\nHorários cadastrados:")
+
+                    for codigo in horarios:
+                        print(codigo, "-", horarios[codigo])
+
             elif n == "3":
 
-                for i in range(len(horarios)):
-                    print(i, "-", horarios[i])
+                if len(horarios) == 0:
+                    print("Nenhum horário cadastrado.")
 
-                pos = int(input("Digite o número do horário: "))
+                else:
 
-                horarios.pop(pos)
+                    for codigo in horarios:
+                        print(codigo, "-", horarios[codigo])
 
-                print("Horário removido.")
+                    codigo = int(input("Digite o código do horário: "))
 
-   
-    # 4- Relatório
-    
+                    horarios.pop(codigo)
+
+                    print("Horário removido com sucesso!")
+
+            elif n == "4":
+
+                if len(usuarios) == 0:
+                    print("Cadastre usuários primeiro.")
+
+                elif len(cortes) == 0:
+                    print("Cadastre cortes primeiro.")
+
+                elif len(horarios) == 0:
+                    print("Cadastre horários primeiro.")
+
+                else:
+
+                    print("\nUsuários:")
+
+                    for cpf in usuarios:
+                        print(
+                            "CPF:", cpf,
+                            "| Nome:", usuarios[cpf][0]
+                        )
+
+                    cpf = input("Digite o CPF do cliente: ")
+
+                    if cpf in usuarios:
+
+                        print("\nCortes:")
+
+                        for codigo in cortes:
+                            print(codigo, "-", cortes[codigo])
+
+                        cod_corte = int(input("Código do corte: "))
+
+                        print("\nHorários:")
+
+                        for codigo in horarios:
+                            print(codigo, "-", horarios[codigo])
+
+                        cod_horario = int(input("Código do horário: "))
+
+                        codigo = len(agendamentos) + 1
+
+                        agendamentos[codigo] = {
+                            "cpf": cpf,
+                            "cliente": usuarios[cpf][0],
+                            "telefone": usuarios[cpf][1],
+                            "corte": cortes[cod_corte],
+                            "horario": horarios[cod_horario]
+                        }
+
+                        print("Agendamento realizado com sucesso!")
+
+                    else:
+                        print("CPF não cadastrado.")
+
+    # ==========================
+    # RELATÓRIO
+    # ==========================
+
     elif m == "4":
 
         print("""
-        ---------------------------
-        -------- RELATÓRIO --------
-        ---------------------------
-        """)
-      
-        print("\nQuantidade de usuários:", len(usuarios))
-        print("Usuários: ", usuarios)
-        print("\nQuantidade de cortes:", len(cortes))
-        print("Cortes: ", cortes)
-        print("\nQuantidade de horários:", len(horarios))
-        print("Horários: ", horarios)
+---------------------------
+-------- RELATÓRIO --------
+---------------------------
+""")
 
-    
-    # Para Sair
-    
+        print("\nUSUÁRIOS")
+
+        for cpf in usuarios:
+            print(
+                "CPF:", cpf,
+                "| Nome:", usuarios[cpf][0],
+                "| Telefone:", usuarios[cpf][1]
+            )
+
+        print("\nCORTES")
+
+        for codigo in cortes:
+            print(codigo, "-", cortes[codigo])
+
+        print("\nHORÁRIOS")
+
+        for codigo in horarios:
+            print(codigo, "-", horarios[codigo])
+
+        print("\nAGENDAMENTOS")
+
+        if len(agendamentos) == 0:
+
+            print("Nenhum agendamento realizado.")
+
+        else:
+
+            for codigo in agendamentos:
+
+                print(
+                    codigo,
+                    "| CPF:", agendamentos[codigo]["cpf"],
+                    "| Cliente:", agendamentos[codigo]["cliente"],
+                    "| Telefone:", agendamentos[codigo]["telefone"],
+                    "| Corte:", agendamentos[codigo]["corte"],
+                    "| Horário:", agendamentos[codigo]["horario"]
+                )
+
+    # ==========================
+    # SAIR E SALVAR
+    # ==========================
+
     elif m == "0":
+
+        dados = {
+            "usuarios": usuarios,
+            "cortes": cortes,
+            "horarios": horarios,
+            "agendamentos": agendamentos
+        }
+
+        arq = open("barbearia.dat", "wb")
+        pickle.dump(dados, arq)
+        arq.close()
+
         print("""
-        Obrigado pela a preferência. 
-        Até a próxima! 
-        """)
+Obrigado pela preferência!
+Até a próxima!
+""")
 
     else:
         print("Opção inválida!")
+
